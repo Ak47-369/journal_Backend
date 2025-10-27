21a2c783d8aa855888a419341bed9aaacea1c1e3
Amit Yadav <akydv2627@gmail.com>
2025-10-27 17:35:06 +0530

    Refactor : Using Lombok

--- journalApp/src/main/java/com/myjournal/journalApp/entity/JournalEntry.java
+++ journalApp/src/main/java/com/myjournal/journalApp/entity/JournalEntry.java
@@ -1,50 +1,20 @@
 package com.myjournal.journalApp.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.bson.types.ObjectId;
 
 // POJO class - Plain Old Java Object
+@Data
+@AllArgsConstructor
 public class JournalEntry {
     private ObjectId id;
     private String title;
     private String content;
 
     // Deafult constructor is needed for Jackson (to convert JSON to Journal Entry)
-    public JournalEntry(){
-
-    }
-
-    public JournalEntry(ObjectId id, String title, String content){
-        this.content = content;
-        this.id  = id;
-        this.title = title;
-    }
-
     public JournalEntry(String content, String title){
         this.content = content;
         this.title = title;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
     }
 }
--- journalApp/src/main/java/com/myjournal/journalApp/entity/JournalEntryEntity.java
+++ journalApp/src/main/java/com/myjournal/journalApp/entity/JournalEntryEntity.java
@@ -1,5 +1,6 @@
 package com.myjournal.journalApp.entity;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
@@ -9,6 +10,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntryEntity {
     @Id
     private ObjectId id;
@@ -20,59 +22,9 @@ public class JournalEntryEntity {
     @LastModifiedDate
     private LocalDateTime updatedAt;
 
-    // Similar to Jackson, Entity also need default constructor for creating object from database data
-    public JournalEntryEntity(){
-
-    }
-
+//    // Similar to Jackson, Entity also need default constructor for creating object from database data
     public JournalEntryEntity(String content, String title) {
         this.title = title;
         this.content = content;
-    }
-
-    public JournalEntryEntity(ObjectId id, String content, String title) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    public ObjectId getId(){
-        return id;
-    }
-
-    public void setId(ObjectId id){
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
     }
 }
