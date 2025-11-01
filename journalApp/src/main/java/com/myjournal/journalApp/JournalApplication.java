50d909b368f2cbeecc14d1006351b29bc11c4561
Amit Yadav <akydv2627@gmail.com>
2025-11-02 00:04:52 +0530

    Refactor(EmailService) : Make email sending async

--- journalApp/src/main/java/com/myjournal/journalApp/JournalApplication.java
+++ journalApp/src/main/java/com/myjournal/journalApp/JournalApplication.java
@@ -6,10 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableAsync
 @EnableMongoAuditing // Now everytime you save() and entity , createdAt and updatedAt fields populated automatically
 //@EnableTransactionManagement - Spring automatically setups it, after detecting spring-boot-starter-data-mongodb
 public class JournalApplication {
--- journalApp/src/main/java/com/myjournal/journalApp/service/EmailService.java
+++ journalApp/src/main/java/com/myjournal/journalApp/service/EmailService.java
@@ -3,6 +3,7 @@ package com.myjournal.journalApp.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,15 +13,17 @@ public class EmailService {
     public EmailService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
+
+    @Async
     public void sendEmail(String to, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
         message.setSubject(subject);
         message.setText(body);
-        log.info("Sending email to: {}", to);
+        log.info("Sending email...")
         try{
             mailSender.send(message);
-            log.info("Email sent to: {}", to);
+            log.info("Email sent!");
         }catch (Exception e){
             log.error("Error while sending mail : {}", e.getMessage());
         }
